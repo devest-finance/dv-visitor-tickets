68042e3676aae7495d22133f580ee16e238b8916
sk263 <simon.kosmrl@gmail.com>
2024-02-23 13:45:04 +0100

    Fetching root id

--- contracts/DvVisitorTicket.sol
+++ contracts/DvVisitorTicket.sol
@@ -11,10 +11,13 @@ contract DvVisitorTicket is DvTicket {
 
     uint256 _rootId;
 
-    constructor(string memory name, string memory symbol, uint256 rootId, address owner, address factory) DvTicket(name, symbol, owner, factory) {
-        _rootId = rootId;
+    constructor(string memory name, string memory symbol, uint256 __rootId, address owner, address factory) DvTicket(name, symbol, owner, factory) {
+        _rootId = __rootId;
     }
 
+    function rootId() public view returns (uint256) {
+        return _rootId;
+    }
 
     // Only publisher can issue a ticket
     function issueNFT(address to, uint256 tokenId, string memory uri)
